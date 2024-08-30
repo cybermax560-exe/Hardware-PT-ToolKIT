a57eba252192ad3c0b8df12119d216271ff653ca
cybermax <debanjansahaofficial360@gmail.com>
2024-08-30 14:23:10 +0530

    0.1v

--- Hardware-PT-ToolKIT.sh
+++ Hardware-PT-ToolKIT.sh
@@ -23,31 +23,28 @@ echo "Upgrading packages..."
 sudo apt-get upgrade -y
 echo "Packages upgraded."  
 
-# Installing lolcat (used for color output)
-sudo apt install -y lolcat
-
 # Function to check if a package is installed
 check_install() {
     if dpkg -l | grep -q "^ii  $1 "; then
-        echo "$1 is already installed." | lolcat
+        echo "$1 is already installed." 
     else
-        echo "$1 is not installed."
-        echo "Installing $1..." | lolcat
+        echo -e "\e[31m $1 is not installed. \e[0m"
+        echo -e "\e[32m Installing $1...  \e[0m" 
         sudo apt-get install -y "$1"
 
         # Check if the installation was successful
         if dpkg -l | grep -q "^ii  $1 "; then
-            echo "$1 installed successfully." | lolcat
+            echo -e "\e[32m $1 installed successfully.  \e[0m"
         else
-            echo "Failed to install $1. Please check for errors above." | lolcat
+            echo "Failed to install $1. Please check for errors above."
         fi
     fi
 }
 
 # Install basic tools if not installed
-basic_tools=(git python3-pip curl wget openjdk-17-jdk p7zip-full zip lolcat)
+basic_tools=(git python3-pip curl wget openjdk-17-jdk p7zip-full zip )
 
-echo "Checking and installing basic tools..." | lolcat
+echo -e "\e[32m Checking and installing basic tools... \e[0m" 
 for tool in "${basic_tools[@]}"; do
     check_install "$tool"
 done
@@ -55,18 +52,17 @@ done
 # Install main tools
 main_tools=(flashrom minicom picocom sigrok pulseview binwalk wireshark nmap esptool screen bettercap ettercap-graphical john hashcat)
 
-echo "Checking and installing main tools..." | lolcat 
+echo -e "\e[32m Checking and installing main tools... \e[0m"
 for tool in "${main_tools[@]}"; do
     check_install "$tool"
 done
 
 # Additional steps after installation
-echo "All specified tools are installed and ready to use." | lolcat
-
+echo -e "\e[32m All specified tools are installed and ready to use.  \e[0m"
 # Reminder: Configure non-root access to Wireshark if necessary
 if [ "$USER" != "root" ]; then
-    echo "If using Wireshark as a non-root user, you may need to configure permissions:"
-    echo "sudo usermod -aG wireshark $USER"
+    echo -e "\e[32m If using Wireshark as a non-root user, you may need to configure permissions: \e[0m"
+    echo -e "\e[32m sudo usermod -aG wireshark $USER  \e[0m"
 fi
 
-echo "Happy Hacking And Fuck The Hardware!" | lolcat
+echo -e "\e[31m Happy Hacking And Fuck The Hardware!  \e[0m"
